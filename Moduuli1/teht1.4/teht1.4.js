d8e28eaf55507e6c8d413881d70b99230448ae4c
eemiljka <eemiljka@metropolia.fi>
2022-11-11 11:38:01 +0200

    teht1.4DONE

--- Moduuli1/teht1.4/teht1.4.js
+++ Moduuli1/teht1.4/teht1.4.js
@@ -4,6 +4,16 @@ let target = document.querySelector('#target')
 let name = prompt("What is your name?")
 
 let house = Math.floor((Math.random() * 4) + 1);
-if (house ==4) {
 
+if (house == 1) {
+target.innerHTML = name + ", you are Gryffindor."
+}
+else if (house == 2) {
+target.innerHTML = name + ", you are Slytherin."
+}
+else if (house == 3) {
+target.innerHTML = name + ", you are Hufflepuff."
+}
+else if (house == 4) {
+target.innerHTML = name + ", you are Ravenclaw."
 }
