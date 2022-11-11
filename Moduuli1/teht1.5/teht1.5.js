0e8be7cbe72da4842cc6222cd1e24e93ae761709
eemiljka <eemiljka@metropolia.fi>
2022-11-11 11:44:11 +0200

    teht1.5DONE

--- Moduuli1/teht1.5/teht1.5.js
+++ Moduuli1/teht1.5/teht1.5.js
@@ -4,3 +4,10 @@ let target = document.querySelector('#target')
 
 let year = parseInt(prompt('Give year:'))
 
+
+if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+    target.innerHTML = year + " is a leap year."
+}
+else {
+    target.innerHTML = year + " is not a leap year."
+}
