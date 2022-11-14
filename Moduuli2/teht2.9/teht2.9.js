25c023014ce376b0187a2cb9719fbacbc7f1a0be
eemiljka <eemiljka@metropolia.fi>
2022-11-14 22:07:37 +0200

    teht2.9DONE

--- Moduuli2/teht2.9/teht2.9.js
+++ Moduuli2/teht2.9/teht2.9.js
@@ -1,8 +1,18 @@
 'use strict';
 let target = document.querySelector('#target')
+let target2 = document.querySelector('#target2')
 
-let numbers = [1, 2, 3, 4]
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+let evenNumbers = []
 
-function even(numbersArr) {
-
+function getEvenNumbers(numbersArr) {
+    for (let i = 0; i < numbersArr.length; i++) {
+        if (numbersArr[i] % 2 === 0) {
+            evenNumbers.push(i+1)
+            target.innerHTML = "Even numbers: " + evenNumbers
+            }
+        }
 }
+
+getEvenNumbers(numbers);
+target2.innerHTML = "Original numbers: " + numbers
