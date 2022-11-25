4e8f907d285e6fb08d74e2f098604eb7474bd715
eemiljka <eemiljka@metropolia.fi>
2022-11-25 13:27:22 +0200

    teht3.3Kesken

--- Moduuli3-module3-starters-main/Moduuli3-module3-starters-main/t3/3.js
+++ Moduuli3-module3-starters-main/Moduuli3-module3-starters-main/t3/3.js
@@ -1,2 +1,10 @@
 'use strict';
+const id = document.querySelector('#target');
 const names = ['John', 'Paul', 'Jones'];
+
+for (var i = 0; i < names.length; i++) {
+
+    console.log(names[i]);
+}
+
+
