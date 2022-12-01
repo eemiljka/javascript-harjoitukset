4701382493c32c8e3f76a8c180168445d490ad9a
eemiljka <eemiljka@metropolia.fi>
2022-12-01 12:54:25 +0200

    teht2.5uusiyritys

--- Moduuli2/teht2.5/teht2.5.js
+++ Moduuli2/teht2.5/teht2.5.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 let target = document.querySelector('#target')
 
 let numbers = [];
@@ -11,4 +11,25 @@ numbers.push(givenNumber)
             break;
         }
     }
-    target.innerHTML = numbers
+    target.innerHTML = numbers */
+
+let target = document.querySelector('#target')
+
+let numbers = [];
+let givenNumber = prompt("Please, enter a number.")
+numbers.push(givenNumber)
+
+let includes = false
+
+while (includes) {
+    let givenNumber = prompt("Please, enter a number.")
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers.includes(i)) {
+            includes = true
+        } else {
+            numbers.push(givenNumber)
+        }
+    }
+
+    numbers.push(givenNumber)
+}
